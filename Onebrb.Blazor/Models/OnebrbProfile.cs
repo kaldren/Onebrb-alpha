8420dc38d2191cadc34d72e50cb645119949258c
kaldren <drenski666@gmail.com>
2020-03-21 16:55:41 +0200

    Onebrb annotations

--- Onebrb.Blazor/Models/OnebrbProfile.cs
+++ Onebrb.Blazor/Models/OnebrbProfile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace Onebrb.Blazor.Models
 {
     public class OnebrbProfile
     {
+        [Required(ErrorMessage = "The first name field is required")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "The last name field is required")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "The email field is required")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "The country field is required")]
         public string Country { get; set; }
+        [Required(ErrorMessage = "The city field is required")]
         public string City { get; set; }
+        [Required(ErrorMessage = "The about field is required")]
         public string About { get; set; }
         public string PhotoPath { get; set; }
     }
