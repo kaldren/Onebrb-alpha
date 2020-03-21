ce29ec6b5cb8329d811abddeeb19381fc669e601
kaldren <drenski666@gmail.com>
2020-03-21 16:29:18 +0200

    OnebrbPhoto

---
+++ Onebrb.Blazor/Models/OnebrbProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onebrb.Blazor.Models
+{
+    public class OnebrbProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string About { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
